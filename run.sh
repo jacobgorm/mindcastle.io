f575dc3073e0fbb74db7721a7733ca7f7c2aae2f
Jacob Gorm Hansen <jacob@grazper.com>
2017-09-25 22:56:30 +0200

    run.sh script

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo umount
+su -c 'umount -f /mnt'
+
+echo pkill
+su -c 'pkill -9 oneroot'
+
+echo rmmod
+su -c 'rmmod nbd && modprobe nbd' || exit 1
+
+echo cleanup state
+su -c 'rm -rf cache/* swapdata-*'
+
+su -c './build/oneroot foo.swap'
